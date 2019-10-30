b3f5862496558a0e1ce83f8290fe2a10bef0b003
liuzan <liuzan@dipont.com>
2019-10-30 15:46:50 +0800

    init

---
+++ src/pages/App/Index.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import './Index.less'
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
+
+const Home = () => {
+  return <Layout>
+    <Header className="header">
+      <span className="logo" >DodAnnie的官方网站</span>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['4']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">React</Menu.Item>
+        <Menu.Item key="2">Vue</Menu.Item>
+        <Menu.Item key="3">Go</Menu.Item>
+        <Menu.Item key="4">LOL</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <Layout style={{ padding: '24px 0', background: '#fff' }}>
+        <Sider width={200} style={{ background: '#fff' }}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%' }}
+          >
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="user" />
+                  subnav 1
+              </span>
+              }
+            >
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="laptop" />
+                  subnav 2
+              </span>
+              }
+            >
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub3"
+              title={
+                <span>
+                  <Icon type="notification" />
+                  subnav 3
+              </span>
+              }
+            >
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="11">option11</Menu.Item>
+              <Menu.Item key="12">option12</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Content style={{ padding: '0 24px', minHeight: 800 }}>Content</Content>
+      </Layout>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>dodannie ©2019 Created by DodAnnie</Footer>
+  </Layout>
+}
+export default Home
