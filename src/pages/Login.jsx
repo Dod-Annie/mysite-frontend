eb697020cb7f163dbbd2efb703ec56d3a28322ba
liuzan <liuzan@dipont.com>
2019-11-08 17:30:21 +0800

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,85 @@
 import React from 'react'
+import ReactCanvasNest from 'react-canvas-nest';
+import { Form, Icon, Input, Button, Checkbox, message } from 'antd'
+import './login.css'
+
+
+class NormalLoginForm extends React.Component {
+  state = {
+    loading: false
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values)
+        this.setState({ loading: true })
+      }
+    })
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <h1 className="login">登录</h1>
+        <Form.Item>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: '请输入您的账号!' }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="手机号或邮箱"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: '请输入您的密码!' }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="密码"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true
+          })(<Checkbox>记住我</Checkbox>)}
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <a className="login-form-forgot" href="">
+            忘记密码
+          </a>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            loading={this.state.loading}
+          >
+            登录
+          </Button>
+          <a href="">没有账号 现在注册</a>
+        </Form.Item>
+      </Form>
+    )
+  }
+}
+
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(
+  NormalLoginForm
+)
+
+
 const Login = () => {
-  return <h1>Login</h1>
+  return <div className="md-warp">
+    <WrappedNormalLoginForm />
+    <ReactCanvasNest className='canvasNest' config={{ pointColor: ' 255, 255, 255 ' }} style={{ zIndex: 0 }} />
+  </div>
 }
 
 export default Login
